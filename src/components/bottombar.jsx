9a8762d0fe211f2eba4b089e6a72b0a84a884967
Sirlam <mdelosab@gmail.com>
2022-09-15 15:02:28 -0300

    iconos bottom bar

--- src/components/bottombar.jsx
+++ src/components/bottombar.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { nav } from '../styles/styles'
 import {Home} from '../pages/Home'
 import { Map } from "../pages/map.jsx";
@@ -10,25 +11,30 @@ import { Register } from "../pages/register";
 
 const Tab = createBottomTabNavigator();
 
-// export function BotBar() {
-//   return (
-//     <NavigationContainer style={nav}>
-//       <Tab.Navigator>
-//         <Tab.Screen name="Inicio" component={Home} />
-//         <Tab.Screen name="Mapa" component={Map} />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   )
-// }
-
 export const BotBar = () => {
   return (
   <NavigationContainer>
     <Tab.Navigator>
-      <Tab.Screen name="Inicio" component={Home} />
-      <Tab.Screen name="Mapa" component={Map} />
-      <Tab.Screen name="Login" component={Login} />
-      <Tab.Screen name="Registro" component={Register} />
+      <Tab.Screen name="Inicio" component={Home} options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color }) => (
+          <MaterialCommunityIcons name="home" color={color} size={26} />),
+        }}/>
+      <Tab.Screen name="Mapa" component={Map} options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color }) => (
+          <MaterialCommunityIcons name="map" color={color} size={26} />),
+        }}/>
+      <Tab.Screen name="Login" component={Login} options={{
+        tabBarLabel: 'Sesion',
+        tabBarIcon: ({ color }) => (
+          <MaterialCommunityIcons name="login" color={color} size={26} />),
+        }}/>
+      <Tab.Screen name="Registro" component={Register} options={{
+        tabBarLabel: 'Registro',
+        tabBarIcon: ({ color }) => (
+          <MaterialCommunityIcons name="registered-trademark" color={color} size={26} />),
+        }}/>
     </Tab.Navigator>
   </NavigationContainer>
   )
